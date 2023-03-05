bb887d3a8b1a3e3244ed587ef57ab57319f140da
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-03-05 18:24:10 -0300

    conclusão front e back para cadastro

---
+++ back_end/crud_kafeitariafit/src/test/java/br/com/escuderodev/kfeitarriafit/crud_kafeitariafit/CrudKafeitariafitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escuderodev.kfeitarriafit.crud_kafeitariafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKafeitariafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
