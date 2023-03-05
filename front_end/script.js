ac90a6413dee03745c4e097571f47ad79c857fb5
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-03-05 16:23:14 -0300

    iniciando o backend

--- front_end/script.js
+++ front_end/script.js
@@ -4,15 +4,35 @@ const Itelefone = document.getElementById("telefone");
 const Iendereco = document.getElementById("endereco");
 const Irestricao = document.getElementById("restricao");
 
+function cadastrar() {
+    fetch("http://localhost:8080/cliente",
+    {
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            nome: Inome.value,
+            telefone: Itelefone.value,
+            endereco: Iendereco.value,
+            restricao: Irestricao.value
+        })
+    })
+    .then(function(res) {console.log(res)})
+    .catch(function(res) {console.log(res)})
+}
+
+function limparCampos() {
+    Inome.value = "";
+    Itelefone.value = "";
+    Iendereco.value = "";
+    Irestricao.value = "";
+}
+
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const dados = {
-        nome: Inome.value,
-        telefone: Itelefone.value,
-        endereco: Iendereco.value,
-        restricao: Irestricao.value
-    };
-
-    console.log(dados);
-})
+    cadastrar();
+    limparCampos();
+});
